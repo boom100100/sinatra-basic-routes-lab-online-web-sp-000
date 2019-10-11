9b156964a9683421fddd5906523678086b64a65d
Bernadette Davis <bsd231@nyu.edu>
2019-10-11 21:44:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ class App < Sinatra::Base
     "Hello, World!"
   end
 
-  get '/' do
+  get '/hometown' do
     "Hello, World!"
   end
 
-  get '/' do
+  get '/favorite-song' do
     "Hello, World!"
   end
 
